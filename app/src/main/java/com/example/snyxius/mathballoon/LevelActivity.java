bc13e0136b53d4c7609dfb10151acb811e3c6fc2
snyxius <soumyamekm@gmail.com>
2015-11-30 18:34:42 +0530

    30/11/15- 6.30

--- app/src/main/java/com/example/snyxius/mathballoon/LevelActivity.java
+++ app/src/main/java/com/example/snyxius/mathballoon/LevelActivity.java
@@ -7,13 +7,16 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 /**
  * Created by Snyxius on 11/16/2015.
  */
 public class LevelActivity extends AppCompatActivity {
-    Button one,two,three,four,five,six,seven,eight,nine;
+    ImageView one,two,three,four,five,six,seven,eight,nine;
     Animation shake;
+    RelativeLayout relative1,relative2,relative3,relative4,relative5,relative6,relative7,relative8,relative9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,25 +26,36 @@ public class LevelActivity extends AppCompatActivity {
         shake.setDuration(1000);
 
 //        animatedView.startAnimation(waveAnimation);
-        one = (Button) findViewById(R.id.balloon11);
-        two = (Button) findViewById(R.id.balloon12);
-        three = (Button) findViewById(R.id.balloon13);
-        four = (Button) findViewById(R.id.balloon21);
-        five = (Button) findViewById(R.id.balloon22);
-        six = (Button) findViewById(R.id.balloon23);
-        seven = (Button) findViewById(R.id.balloon31);
-        eight = (Button) findViewById(R.id.balloon32);
-        nine = (Button) findViewById(R.id.balloon33);
+        one = (ImageView) findViewById(R.id.balloon11);
+        two = (ImageView) findViewById(R.id.balloon12);
+        three = (ImageView) findViewById(R.id.balloon13);
+        four = (ImageView) findViewById(R.id.balloon21);
+        five = (ImageView) findViewById(R.id.balloon22);
+        six = (ImageView) findViewById(R.id.balloon23);
+        seven = (ImageView) findViewById(R.id.balloon31);
+        eight = (ImageView) findViewById(R.id.balloon32);
+        nine = (ImageView) findViewById(R.id.balloon33);
 
-        one.startAnimation(shake);
-        two.startAnimation(shake);
-        three.startAnimation(shake);
-        four.startAnimation(shake);
-        five.startAnimation(shake);
-        six.startAnimation(shake);
-        seven.startAnimation(shake);
-        eight.startAnimation(shake);
-        nine.startAnimation(shake);
+        relative1=(RelativeLayout)findViewById(R.id.relative11);
+        relative2=(RelativeLayout)findViewById(R.id.relative12);
+        relative3=(RelativeLayout)findViewById(R.id.relative13);
+        relative4=(RelativeLayout)findViewById(R.id.relative21);
+        relative5=(RelativeLayout)findViewById(R.id.relative22);
+        relative6=(RelativeLayout)findViewById(R.id.relative23);
+        relative7=(RelativeLayout)findViewById(R.id.relative31);
+        relative8=(RelativeLayout)findViewById(R.id.relative32);
+        relative9=(RelativeLayout)findViewById(R.id.relative33);
+
+
+        relative1.startAnimation(shake);
+        relative2.startAnimation(shake);
+        relative3.startAnimation(shake);
+        relative4.startAnimation(shake);
+        relative5.startAnimation(shake);
+        relative6.startAnimation(shake);
+        relative7.startAnimation(shake);
+        relative8.startAnimation(shake);
+        relative9.startAnimation(shake);
 
 //        one.setR
 
@@ -50,7 +64,77 @@ public class LevelActivity extends AppCompatActivity {
         one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LevelActivity.this, MainActivity.class);
+                Intent intent = new Intent(LevelActivity.this, Rising.class);
+                startActivity(intent);
+
+            }
+        });
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LevelActivity.this, Rising.class);
+                startActivity(intent);
+
+            }
+        });
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LevelActivity.this, Rising.class);
+                startActivity(intent);
+//                findViewById(R.id.balloon11).startAnimation(shake);
+
+            }
+        });
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LevelActivity.this, Rising.class);
+                startActivity(intent);
+//                findViewById(R.id.balloon11).startAnimation(shake);
+
+            }
+        });
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LevelActivity.this, Rising.class);
+                startActivity(intent);
+//                findViewById(R.id.balloon11).startAnimation(shake);
+
+            }
+        });
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LevelActivity.this, Rising.class);
+                startActivity(intent);
+//                findViewById(R.id.balloon11).startAnimation(shake);
+
+            }
+        });
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LevelActivity.this, Rising.class);
+                startActivity(intent);
+//                findViewById(R.id.balloon11).startAnimation(shake);
+
+            }
+        });
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LevelActivity.this, Rising.class);
+                startActivity(intent);
+//                findViewById(R.id.balloon11).startAnimation(shake);
+
+            }
+        });
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LevelActivity.this, Rising.class);
                 startActivity(intent);
 //                findViewById(R.id.balloon11).startAnimation(shake);
 
